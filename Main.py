4903182b632565e0526ee26bb05f39fc82513b2f
wenye199100 <two_ye@hotmail.com>
2018-06-27 12:51:31 +0800

    version1

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 #coding=utf-8
 import json
 from selenium import webdriver
+import datetime
 
 def load_config():
     config_path = "config.json"
@@ -8,7 +9,16 @@ def load_config():
     settings = json.load(f)
     return settings
 
+def CheckIn():
+    settings = load_config()
+    dr = webdriver.Firefox()
+    dr.get(settings['url']['login'])
+    dr.find_element_by_id('username').send_keys(settings['personal-info']['erp-account'])
+    dr.find_element_by_id('password').send_keys(settings['personal-info']['erp-password'])
+    dr.find_element_by_class_name('formsubmit_btn').click()
+    dr.find_element_by_id('clockLink').click()
+    dr.quit()
+
 
 if __name__ == "__main__":
-    dr = webdriver.Chrome('C:\\Users\\qzlab\\Documents\\GitHub\\chromedriver.exe')
-    settings = load_config()
+    CheckIn()
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # JD-CheckIn
+windows定时器 https://blog.csdn.net/shengerjianku/article/details/78872082
+driver（firefox）下载地址 https://github.com/mozilla/geckodriver/releases
+需要自己配置config.json的文件
